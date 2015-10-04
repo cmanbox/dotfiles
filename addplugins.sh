aa2262b9af0ef1fbfebe126a32b1cc5666cb832b
chris <mollycm1972@bigpond.com>
2015-10-04 13:57:09 +1100

    install railscasts.vim into colors

--- addplugins.sh
+++ addplugins.sh
@@ -12,3 +12,6 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
 git clone https://github.com/scrooloose/nerdtree.git
+
+mkdir -p ~/.vim/colors
+mv ~/dotfiles/railscasts.vim ~/.vim/colors
