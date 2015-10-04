6a5ed02396446ed52368180199fc74953939a19b
chris <mollycm1972@bigpond.com>
2015-10-04 13:45:23 +1100

    bug fix

--- addplugins.sh
+++ addplugins.sh
@@ -12,5 +12,3 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
 git clone https://github.com/scrooloose/nerdtree.git
-cd ~/.vim
-git submodule add git@github.com:jpo/vim-railscasts-theme.git bundle/railscasts-theme
