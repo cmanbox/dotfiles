df7ee97c814f21832890ce7d0274ec5a05900e77
chris <mollycm1972@bigpond.com>
2015-10-04 13:26:10 +1100

    Add NERDtree

--- addplugins.sh
+++ addplugins.sh
@@ -10,4 +10,5 @@
 mkdir ~/dotfiles/vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
+cd ~/.vim/bundle
+git clone https://github.com/scrooloose/nerdtree.git
